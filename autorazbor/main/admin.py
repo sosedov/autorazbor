eaaf6e54be014d9b3bfb3a0c7069ab0694071267
freezywell <deni_g7@mail.ru>
2022-07-18 23:25:45 +0300

    добавление моделей, создание администратора

--- autorazbor/main/admin.py
+++ autorazbor/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Auto, Auto_Model, Auto_SubModel, Part, Part_Model
 # Register your models here.
+admin.site.register(Auto)
+admin.site.register(Auto_Model)
+admin.site.register(Auto_SubModel)
+admin.site.register(Part)
+admin.site.register(Part_Model)
