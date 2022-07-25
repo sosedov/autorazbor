35905f6b7151ce6b3322e701d5bf4328bffe5820
freezywell <deni_g7@mail.ru>
2022-07-25 17:37:02 +0300

    any comment

--- autorazbor/cars/views.py
+++ autorazbor/cars/views.py
@@ -1,3 +1,18 @@
+from http.client import REQUEST_URI_TOO_LONG
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import CarMark, CarModel, CarSubmodel
+
+class Cars(View):
+    def GetCarMark(request):
+        mark = CarMark.objects.all()
+        return render(request, 'main/search.html', {'mark': mark})
+
+    def GetCarModel(request):
+        model = CarModel.objects.all()
+        return render(request, 'main/search.html', {'model': model})
+
+    def GetCarSubmodel(request):
+        submodel = CarSubmodel.objects.all()
+        return render(request, 'main/search.html', {'submodel': submodel})
--- autorazbor/main/views.py
+++ autorazbor/main/views.py
@@ -1,5 +1,8 @@
+from pickletools import markobject
 from django.shortcuts import render
 
+from cars.models import CarMark, CarModel, CarSubmodel
+from parts.models import PartCategory, PartSubCategory, Parts
 # Create your views here.
 def authorization(request):
     return render(request, 'main/authorization.html')
@@ -8,7 +11,16 @@ def usercab(request):
     return render(request, 'main/usercab.html')
 
 def search(request):
-    return render(request, 'main/search.html')
+    marks = CarMark.objects.all()
+    models = CarModel.objects.all()
+    submodels = CarSubmodel.objects.all()
+    parts = Parts.objects.all()
+    return render(request, 'main/search.html', {
+        'mark': marks,
+        'model': models,
+        'submodel': submodels,
+        'parts': parts,
+        })
 
 def parts(request):
     return render(request, 'main/parts.html')
